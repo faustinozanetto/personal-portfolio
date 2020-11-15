50b7188c30b2d1735dd0043f3ccd3e35f37776f1
Retrosen <faustizanetto@gmail.com>
2020-11-15 00:27:46 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Portfolio = styled.div`
+  border: 0;
+  margin: 0;
+  font-family: 'Poppins';
+`;
+
+const App = () => {
+  return (
+    <Portfolio>
+      <h1>Personal Portfolio</h1>
+    </Portfolio>
+  );
+};
+
+export default App;
